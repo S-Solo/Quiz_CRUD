132d1052fea61552a5ec71e37939864c34ee8ddb
S-Solo <38946201+S-Solo@users.noreply.github.com>
2018-05-03 18:38:54 +0400

    Update app.js

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.use((req,res,next) => {
 });
 
 app.use( (error, req, res, next) => {
-    res.status(err.status || 500) ;
+    res.status(error.status || 500) ;
     res.json({
-        error: {
+        Error: {
             message: error.message
         }
     });
